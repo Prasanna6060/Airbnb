9cb20054daa76d9d82ea232338a55266efb5346d
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-03-05 15:04:46 +0545

    setup bcrypt and saved user to database

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,23 @@
 import userModel from '../models/user.models.js';
+import bcrypt from 'bcrypt'
 
 export const getUser = (req, res) => {
     res.send("getting all users")
 }
 
-export const registerUser = (req, res) => {
+export const registerUser = async (req, res) => {
    const { username, email, password } = req.body;
    console.log( username, email, password )
+
+   const saltRounds = 10 ;
+   const passwordHash = await bcrypt.hash(password, saltRounds)
+
+   const newUser = new userModel({
+    username,
+    email,
+    password: passwordHash
+   })
+   console.log(newUser);
+   const savedUser = await newUser.save();
+   res.status(201).json(savedUser);
 }
